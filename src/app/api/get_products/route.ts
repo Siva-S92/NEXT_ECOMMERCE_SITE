f4c7e717b0414fc87aa8c001f6faf867a33252f2
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-07-26 15:49:37 +0530

    update for Next.JS renders the route dynamically!

--- src/app/api/get_products/route.ts
+++ src/app/api/get_products/route.ts
@@ -2,6 +2,7 @@ import { connectMongoDB } from "@/lib/connectMongoDB";
 import Product from "@/lib/models/Product";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
 export async function GET(){
     try {
         await connectMongoDB();
